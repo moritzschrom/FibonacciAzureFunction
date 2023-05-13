643c56b67a31d83776111af1ed9734684f264461
Moritz Schrom <Moritz.Schrom@erstegroup.com>
2023-05-13 22:58:08 +0200

    Remove fastapi

--- api/FibonacciService/__init__.py
+++ api/FibonacciService/__init__.py
@@ -1,27 +1,31 @@
 import logging
+
 import azure.functions as func
-import fastapi
 
-app = fastapi.FastAPI()
 
-@app.get("/FibonacciService")
-async def fibonacci(length: int) -> List[int]:
-    def generate_fibonacci(n):
-        if n <= 0:
-            return []
-        elif n == 1:
-            return [1]
-        elif n == 2:
-            return [1, 1]
+def fibonacci(n):
+    fib_sequence = [0, 1]
+    while len(fib_sequence) < n:
+        fib_sequence.append(fib_sequence[-1] + fib_sequence[-2])
+    return fib_sequence[:n]
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    length = req.params.get('length')
+    if not length:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
         else:
-            fib_sequence = [1, 1]
-            while len(fib_sequence) < n:
-                fib_sequence.append(fib_sequence[-1] + fib_sequence[-2])
-            return fib_sequence
+            length = req_body.get('length')
 
-    return generate_fibonacci(length)
-
-
-async def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
-    """Each request is redirected to the ASGI handler."""
-    return await func.AsgiMiddleware(app).handle_async(req, context)
+    if length:
+        fib_sequence = fibonacci(int(length))
+        return func.HttpResponse(json.dumps(fib_sequence))
+    else:
+        return func.HttpResponse(
+             "Please pass a length parameter on the query string or in the request body",
+             status_code=400
+        )
